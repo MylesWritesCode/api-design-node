22c1f8a39990d610e4a3789a9825c410b8dbaaa0
Myles Berueda <mberueda@codemithril.com>
2020-02-10 08:48:08 -1000

    step-1 finished

--- server.js
+++ server.js
@@ -2,4 +2,22 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+let express = require('express');
+let app = express();
+
+// Send back index.html on a get request to root:
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html', (err) => {
+    if (err) { res.status(500).send(err); }
+  });
+});
+
+// Send back jsonData on a request to /data:
+var jsonData = { count: 12, message: 'hey' };
+
+app.get('/data', (req, res) => {
+  res.status(200).json(jsonData);
+});
+
+
+app.listen(3000);
